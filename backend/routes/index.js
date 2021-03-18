d679856b9c0c11f79fafbd16feba72fc5d9a2e94
nurbolatk <nurbolattk@gmail.com>
2021-03-18 18:23:39 +0600

    Change register endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,52 +1,52 @@
-const express = require('express');
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-const { register, getUser, updateSymptoms } = require('../services/userService')
-const { writeSensors, getAllSensors } = require('../services/sensorService')
+const {
+  register,
+  getUser,
+  updateSymptoms,
+} = require("../services/userService");
+const { writeSensors, getAllSensors } = require("../services/sensorService");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 router.post("/register", async (req, res, next) => {
   try {
-    
-    const doc = await register(req.body)
-    console.log("doc")
-    console.log(doc)
-    res.send(String(doc))
-
+    const doc = await register(req.body);
+    console.log("doc");
+    console.log(doc);
+    res.status(201).json(doc);
   } catch (error) {
-    console.log("error")
-    console.log(error)
-    res.send(error)
-
+    console.log("error");
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
   }
-
 });
 
 router.post("/predict", async (req, res, next) => {
   try {
     const metadata = await getUser(req.body.id);
-    const totaldata = Object.assign({}, metadata._doc, req.body)
-    const result = await axios.post('http://localhost:5000', totaldata);
-    res.send(String(result.data))
+    const totaldata = Object.assign({}, metadata._doc, req.body);
+    const result = await axios.post("http://localhost:5000", totaldata);
+    res.send(String(result.data));
   } catch (error) {
-    console.error(error.stack)
-    res.send(500)
+    console.error(error.stack);
+    res.send(500);
   }
 });
 
 router.get("/metadata", async (req, res, next) => {
   try {
     const doc = await getUser(req.query.id);
-    res.send(doc)
+    res.send(doc);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
 /* {
   _id: 0,
@@ -57,7 +57,6 @@ router.post("/spirometer", async (req, res, next) => {
   try {
     console.log(req.body);
 
-
     const data = await getUser(req.query._id);
     const gender = data.sex;
 
@@ -65,58 +64,62 @@ router.post("/spirometer", async (req, res, next) => {
     if (req.body.sensor_type == "spirometer") {
       //if fev1
       if (gender == "male" && req.body.sensor_value < 3.5) {
-        doc = await updateSymptoms(req.body._id, { "pneumonia": 1, "difficult_breathing": 1 });
-      }
-      else if (gender == "female" && req.body.sensor_value < 2.5) {
-        doc = await updateSymptoms(req.body._id, { "pneumonia": 1, "difficult_breathing": 1 });
+        doc = await updateSymptoms(req.body._id, {
+          pneumonia: 1,
+          difficult_breathing: 1,
+        });
+      } else if (gender == "female" && req.body.sensor_value < 2.5) {
+        doc = await updateSymptoms(req.body._id, {
+          pneumonia: 1,
+          difficult_breathing: 1,
+        });
       }
     }
-
 
     //CHEST PAIN ML
     //HEADACKE TRESHOLD HIGH BLOOD PRESSURE
     res.send(doc);
-
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
 //NEED TO BE TESTED
-router.post("/pulseoximeter", async (req, res, next) => {
+router.get("/spo", async (req, res, next) => {
   try {
-    console.log(req.body);
+    console.log(req.query);
     //SEND TO ML SERVICE
-    const result = await axios.post('http://localhost:5000', req.body.sensor_value);
+    const result = await axios.post("http://localhost:5000", req.query.s);
     if (result == 1) {
-      let doc = await updateSymptoms(req.body.id, { "fatigue": 1 });
+      let doc = await updateSymptoms(req.query.i, { fatigue: 1 });
       res.send(doc);
     } else {
-      console.log("Fatigue not predicted")
+      console.log("Fatigue not predicted");
     }
-
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
 //NEED TO BE TESTED
 router.post("/thermometer", async (req, res, next) => {
   try {
     console.log(req.body);
     //SEND TO ML SERVICE
-    const result = await axios.post('http://localhost:5000', req.body.sensor_value);
+    const result = await axios.post(
+      "http://localhost:5000",
+      req.body.sensor_value
+    );
     if (result == 1) {
-      let doc = await updateSymptoms(req.body.id, { "fever": 1 });
+      let doc = await updateSymptoms(req.body.id, { fever: 1 });
       res.send(doc);
     } else {
-      console.log("Fever not predicted")
+      console.log("Fever not predicted");
     }
-
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
 //NEED TO BE TESTED
 // HERE SHOULD BE CHEST PAIN
@@ -131,8 +134,6 @@ router.post("/thermometer", async (req, res, next) => {
 }
 */
 
-router.post("/")
-
-
+router.post("/");
 
 module.exports = router;
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,53 +1,48 @@
-const { User } = require('../schemas/user')
-const mongoose = require('mongoose');
+const { User } = require("../schemas/user");
+const mongoose = require("mongoose");
 
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcrypt");
 
 const register = async (metadata) => {
-    let doc;
-    User.findOne({ "email": metadata.email }, function (err, user) {
-        if (user) {
-            console.log("email exists?")
-            return err
-        } else {
-            doc = new User({
-                password: bcrypt.hashSync(metadata.password, 8),
-                email: metadata.email,
-                name: metadata.name,
-                age: metadata.age,
-                gender: metadata.gender
-            })
-            doc.save();
-            console.log(doc);
-            return doc;
-        }
-    })
+  const user = await User.findOne({ email: metadata.email });
 
-
-}
+  if (user) {
+    console.log("email exists?");
+    throw new Error("email already exists");
+  } else {
+    const doc = new User({
+      password: bcrypt.hashSync(metadata.password, 8),
+      email: metadata.email,
+      name: metadata.name,
+      age: metadata.age,
+      gender: metadata.gender,
+    });
+    const results = await doc.save();
+    console.log(results);
+    return results;
+  }
+};
 
 const getUser = async (id) => {
-    try {
-
-        const doc = await User.findById(id);
-        console.log(doc);
-        return doc;
-    } catch (error) {
-        return error
-    }
-
-}
+  try {
+    const doc = await User.findById(id);
+    console.log(doc);
+    return doc;
+  } catch (error) {
+    return error;
+  }
+};
 
 const updateSymptoms = async (id, sensors_data) => {
-    try {
-        const doc = await User.findOneAndUpdate({ _id: id }, sensors_data);
-        doc.save()
-        console.log(doc);
-        return doc;
-    } catch (error) {
-        return error
-    }
-}
+  try {
+    const doc = await User.findOneAndUpdate({ _id: id }, sensors_data);
+    doc.save();
+    console.log(doc);
+    return doc;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports.register = register;
 module.exports.getUser = getUser;

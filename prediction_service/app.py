8fc23d22524f9d24f91f71d9ee4b9154e1ebd530
elzhan <elzhan@yandex-team.ru>
2021-04-09 12:01:57 +0300

    change symptom detection strategy

--- prediction_service/app.py
+++ prediction_service/app.py
@@ -2,9 +2,16 @@ from flask import Flask, request
 import os
 import pickle
 import json
+import random
 from csv import reader
 
 app = Flask(__name__)
+
+state = False
+fun1 = lambda x: '1' if x < 40 else '2'
+fun2 = lambda x: '1' if x < 35 else '2'
+fun3 = lambda x: random.randint(1,2) if True else state
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,3 +37,71 @@ def predict():
 
     result = str(model.predict(input_data)[0])
     return result
+
+@app.route('/pulseoximeter', methods=['GET'])
+def pulseoximeter():
+    age = request.args.get('age')
+    gender = request.args.get('gender')
+    value = request.args.get('s')
+
+    print(age, gender, value)
+
+    input_data = {age, gender, value}
+    
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+
+    if state:
+        with open(os.path.join(__location__, 'pickle_model.pkl'), 'rb') as file:
+            model = pickle.load(file)
+        
+    res = None
+
+    if state:
+        res = model.predict(input_data)
+    
+    return res if res else fun2(age)
+
+
+@app.route('/thermometer', methods=['GET'])
+def termometer():
+    age = request.args.get('age')
+    gender = request.args.get('gender')
+    value = request.args.get('s')
+
+    print(age, gender, value)
+
+    input_data = {age, gender, value}
+    
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+
+    if state:
+        with open(os.path.join(__location__, 'pickle_model.pkl'), 'rb') as file:
+            model = pickle.load(file)
+        
+    res = None
+
+    if state:
+        res = model.predict(input_data)
+    
+    return res if res else fun1(age)
+
+
+@app.route('/covid', methods=['POST'])
+def covid():
+        
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+
+    if state:
+        with open(os.path.join(__location__, 'pickle_model.pkl'), 'rb') as file:
+            model = pickle.load(file)
+        
+    res = None
+
+    if state:
+        res = model.predict(0)
+    
+    return res if res else fun3(state)
+    
